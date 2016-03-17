21c9ab18e137eaa2a80b755cf16e932e27de535b
Naveen <p6naveen@gmail.com>
2016-03-17 19:39:27 +0530

    src folder added

---
+++ src/User.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * User methods
+ * @author Naveen
+ */
+
+namespace BambooTree;
+
+class User {
+
+    /**
+     * Constructor
+     */
+    function __construct() {
+        
+    }
+
+    /**
+     * 
+     * @param string $username
+     * @param string $password
+     * @return array
+     */
+    public function Login($username, $password) {
+        
+    }
+
+}
